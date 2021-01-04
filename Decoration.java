f7dedd70956967b9d89258f0a8172cf2d6df8eaf
RANJITHA888 <76859900+RANJITHA888@users.noreply.github.com>
2021-01-04 11:24:52 +0530

    Add files via upload

---
+++ Decoration.java
@@ -0,0 +1,14 @@
+class Decoration
+{
+	public static void main(String a[])
+{
+    int num=300;
+    float price=145.50f;
+    String name="flower";
+    String location="banglore";
+   System.out.println(num);
+   System.out.println(price);
+   System.out.println(name);
+   System.out.println(location);
+ }
+ }
