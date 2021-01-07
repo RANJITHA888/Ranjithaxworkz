c092e02aaf2b728d240781f2d7c1221e2881a205
ranjitha b m <ranjithabm424@gmail.com>
2021-01-07 13:24:12 +0530

    adding the programs of arrays

---
+++ Malls1.java
@@ -0,0 +1,13 @@
+class Malls1
+{
+static int noOfShopes=60;
+static String shopNames[]={"zudio","trends","max","peter england"};
+public static void main(String a[])
+{
+System.out.println(noOfShopes);
+System.out.println(shopNames[0]);
+System.out.println(shopNames[1]);
+System.out.println(shopNames[2]);
+System.out.println(shopNames[3]);
+}
+}
